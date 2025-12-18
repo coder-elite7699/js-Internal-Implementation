53f494e7906d1798d0cf5455a2551f334c1d3d34
pk576 <pkpc22042023@gmail.com>
2025-12-18 10:58:00 +0530

    full implementation of typeof

--- typeofImp/typeof.c
+++ typeofImp/typeof.c
@@ -1,6 +1,90 @@
 #include<stdio.h>
-int main(){
+typedef enum {
+    JS_NUMBER,
+    JS_STRING,
+    JS_BOOLEAN,
+    JS_UNDEFINED,
+    JS_NULL,
+    JS_OBJECT,
+    JS_FUNCTION,
+    JS_SYMBOL,
+    JS_BIGINT
+} JSType;
 
-    printf("Hello");
+typedef struct {
+    JSType type;   // TAG
+    union {
+        double number;
+        char* string;
+        int boolean;
+        void* object;
+    } data;
+} JSValue;
+
+JSValue js_number(double n) {
+    JSValue v;
+    v.type = JS_NUMBER;
+    v.data.number = n;
+    return v;
+}
+
+JSValue js_string(char* s) {
+    JSValue v;
+    v.type = JS_STRING;
+    v.data.string = s;
+    return v;
+}
+
+JSValue js_undefined() {
+    JSValue v;
+    v.type = JS_UNDEFINED;
+    return v;
+}
+
+JSValue js_null() {
+    JSValue v;
+    v.type = JS_NULL;
+    return v;
+}
+const char* js_typeof(JSValue v) {
+    switch (v.type) {
+        case JS_NUMBER:
+            return "number";
+
+        case JS_STRING:
+            return "string";
+
+        case JS_BOOLEAN:
+            return "boolean";
+
+        case JS_UNDEFINED:
+            return "undefined";
+
+        case JS_SYMBOL:
+            return "symbol";
+
+        case JS_BIGINT:
+            return "bigint";
+
+        case JS_FUNCTION:
+            return "function";
+
+        case JS_OBJECT:
+            return "object";
+
+        case JS_NULL:
+            return "object"; // historical JS bug
+
+        default:
+            return "unknown";
+    }
+}
+#include <stdio.h>
+
+int main() {
+    JSValue a = js_number(10);
+
+    printf("typeof a = %s\n", js_typeof(a));
+
     return 0;
 }
